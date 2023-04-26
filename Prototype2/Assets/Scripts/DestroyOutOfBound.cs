0e50c1a90a8ee3243a5908925c9f1fe7ffe2101d
Taihou F <59370291+AnsonFeng1999@users.noreply.github.com>
2023-04-26 15:38:56 -0400

    Updated prototype 2, finished half of Unit 2.

--- Prototype2/Assets/Scripts/DestroyOutOfBound.cs
+++ Prototype2/Assets/Scripts/DestroyOutOfBound.cs
@@ -15,12 +15,14 @@ public class DestroyOutOfBound : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If an object goes pass player's view, destroy it.
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
--- Prototype2/Assets/Scripts/PlayerController.cs
+++ Prototype2/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 10.0f;
-    public float xRange = 10.0f;
+    public float speed = 10f;
+    public float xRange = 20f;
     public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
