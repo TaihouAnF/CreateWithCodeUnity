1a8353b001604879679aa54e732ab04b91d706c6
Taihou F <59370291+AnsonFeng1999@users.noreply.github.com>
2023-04-27 14:08:24 -0400

    Finished all sets of challenges in project 2, working on bonus.

---
+++ Prototype2/Assets/Scripts/ScriptsC2/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float pressInterval = 1f;
+    private float currentTimeLeft = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (currentTimeLeft <= 0f) 
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                currentTimeLeft = pressInterval;
+            }
+        }
+        currentTimeLeft -= Time.deltaTime;
+        currentTimeLeft = Mathf.Clamp(currentTimeLeft, 0f, pressInterval);
+    }
+
+    
+}
