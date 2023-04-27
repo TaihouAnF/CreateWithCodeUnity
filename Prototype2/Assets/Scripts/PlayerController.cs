32c44db2648244a16ef39bcc9740a99938bcbc0a
Taihou F <59370291+AnsonFeng1999@users.noreply.github.com>
2023-04-27 14:42:06 -0400

    Finished bonus 1(easy level).

--- Prototype2/Assets/Scripts/PlayerController.cs
+++ Prototype2/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10f;
     public float xRange = 20f;
+    public float zRange = 10f;
+    public float bottomZRange = -2f;
     public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
@@ -25,8 +28,19 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+        if (transform.position.z < bottomZRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, bottomZRange);
+        }
+        
         horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
--- Prototype2/Assets/Scripts/ScriptsC2/PlayerControllerX.cs
+++ Prototype2/Assets/Scripts/ScriptsC2/PlayerControllerX.cs
@@ -11,15 +11,18 @@ public class PlayerControllerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
+        // On spacebar press, send dog, here we directly use time as the T/F.
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            // If time interval has been passed.
             if (currentTimeLeft <= 0f) 
             {
                 Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                // Reset the interval to wait.
                 currentTimeLeft = pressInterval;
             }
         }
+        // Update the time, it should not be in the last "if", otherwise it would only update when spacebar is pressed
         currentTimeLeft -= Time.deltaTime;
         currentTimeLeft = Mathf.Clamp(currentTimeLeft, 0f, pressInterval);
     }
