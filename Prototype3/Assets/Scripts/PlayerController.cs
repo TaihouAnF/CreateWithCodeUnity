1c60531179e9452cd241eb72aaa60060dad22cff
Taihou F <59370291+AnsonFeng1999@users.noreply.github.com>
2023-05-08 16:14:34 -0400

    Added death animation.

--- Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -39,6 +39,8 @@ public class PlayerController : MonoBehaviour
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             isGameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
             Debug.Log("Game Over");
         }
         
