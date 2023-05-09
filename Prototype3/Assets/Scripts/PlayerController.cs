bc3904012b1697690e2c04ac836affbf32ff5947
Taihou F <59370291+AnsonFeng1999@users.noreply.github.com>
2023-05-09 15:34:00 -0400

    Adjusted if-statement for double jumping, as double jumping is a bit different than jumping on the ground, so I added another clause for it. Now the animation should play normally.

--- Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private Animator playerAnim;
     private AudioSource playerAudio;
-    public float jumpForce = 8;
+    public float jumpForce = 500;
+    public float doubleJumpForce = 400;
     public float gravityModifier = 1;
     public bool isOnGround = true;
     public bool doubleJump = true;
@@ -29,16 +30,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && (isOnGround || doubleJump) && !isGameOver)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !isGameOver)
         {
-            if (!isOnGround) doubleJump = false;
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
             playerAudio.PlayOneShot(jumpSound, 1.0f);
             isOnGround = false;
-            
         }
+        else if (Input.GetKeyDown(KeyCode.Space) && !isOnGround && doubleJump && !isGameOver)
+        {
+            playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+            playerAnim.Play("Running_Jump", 3, 0f);
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+            doubleJump = false;
+        }
+
+        // Fixed when after double jumping and hitting obstacles and still playing dirt.
         if (isGameOver)
         {
             dirtParticle.Stop();
