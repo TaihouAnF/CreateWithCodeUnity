2fe0315ecdaf9ee4a026dd406fbe576d539144c6
Taihou F <59370291+AnsonFeng1999@users.noreply.github.com>
2023-05-09 15:13:29 -0400

    Added double jump and fixed dirt still playing showhow when game ends.

--- Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 8;
     public float gravityModifier = 1;
     public bool isOnGround = true;
+    public bool doubleJump = true;
     public bool isGameOver = false;
     public ParticleSystem explosionParticle;
     public ParticleSystem dirtParticle;
@@ -28,13 +29,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !isGameOver)
+        if (Input.GetKeyDown(KeyCode.Space) && (isOnGround || doubleJump) && !isGameOver)
         {
+            if (!isOnGround) doubleJump = false;
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
             playerAudio.PlayOneShot(jumpSound, 1.0f);
             isOnGround = false;
+            
+        }
+        if (isGameOver)
+        {
+            dirtParticle.Stop();
         }
     }
 
@@ -43,6 +50,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            doubleJump = true;
             dirtParticle.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
