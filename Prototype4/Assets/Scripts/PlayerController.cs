8d9ea58d1c75176c58839031320f6dbcbeabb102
Taihou F <59370291+AnsonFeng1999@users.noreply.github.com>
2023-05-12 15:53:05 -0400

    Adjusted player to go forward in focal point forward direction. Added enemy and physics material to make balls bounce.

--- Prototype4/Assets/Scripts/PlayerController.cs
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -6,16 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     private Rigidbody playerRb;
+    private GameObject focalPoint;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(forwardInput * speed * Vector3.forward);
+        playerRb.AddForce(forwardInput * speed * focalPoint.transform.forward);
     }
 }
