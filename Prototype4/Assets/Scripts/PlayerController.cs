b58ff12ea7d951dd49ee7ed99b35ae18dc9d027e
Taihou F <59370291+AnsonFeng1999@users.noreply.github.com>
2023-05-12 15:02:36 -0400

    Added Player forward control and camera rotation control scripts.

---
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(forwardInput * speed * Vector3.forward);
+    }
+}
