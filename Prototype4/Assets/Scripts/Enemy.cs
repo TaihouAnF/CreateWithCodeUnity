7bccb336b5e4140957b039be91e3247e4ea24e1b
Taihou F <59370291+AnsonFeng1999@users.noreply.github.com>
2023-06-29 16:38:11 -0400

    Added: Boss attributes, boss will spawn minions

--- Prototype4/Assets/Scripts/Enemy.cs
+++ Prototype4/Assets/Scripts/Enemy.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed;
+    public bool isBoss;
+    public int minionCount;
+    [SerializeField] private float spawnInterval;
+    private float nextSpawn;
     private Rigidbody enemyRb;
     private GameObject player;
+    private SpawnManager spawnManager;
     // Start is called before the first frame update
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
+        if (isBoss) 
+        { 
+            spawnManager = FindObjectOfType<SpawnManager>();
+        }
     }
 
     // Update is called once per frame
@@ -19,6 +28,15 @@ public class Enemy : MonoBehaviour
     {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(speed * lookDirection);
+        if (isBoss && spawnManager != null)
+        {
+            if (Time.time > nextSpawn)
+            {
+                nextSpawn = Time.time + spawnInterval;
+                spawnManager.SpawnMiniEnemy(minionCount);
+            }
+        }
+
         if (transform.position.y < -10)
         {
             Destroy(gameObject);
