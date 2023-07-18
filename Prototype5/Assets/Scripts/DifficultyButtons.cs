a3121216594fda7ee91f25955539349bc80288b5
Taihou F <59370291+AnsonFeng1999@users.noreply.github.com>
2023-07-18 17:31:48 -0400

    Add logic to buttons

---
+++ Prototype5/Assets/Scripts/DifficultyButtons.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButtons : MonoBehaviour
+{
+    private Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetDifficulty()
+    {
+        Debug.Log(gameObject.name + " was clicked");
+    }
+}
